aa76a0e224cf8b47c783f9eafc531309858a75bb
poojagautamcoder <poojagautam@excellencetechnologies.in>
2022-02-21 18:20:32 +0530

    authantication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Signup from "./Pages/SignUp";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 function App() {
   return (
-    <div className="App">
-     <Signup/>
-    </div>
+    <Container style={{ width: "400px" }}>
+      <Row>
+        <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
